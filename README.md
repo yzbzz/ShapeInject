1b0a473d5d67e02f2ea6468dfcdc497083eb618a
liuhongzhe <liuhongzhe@etcp.cn>
2018-08-22 11:37:55 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# ShapeInject
-使用ShapeInject，可能很方便给View和ViewGroup设置背景，不用再写selector文件啦
+# StylePhrase - Android string formatting
+
+StylePhrase is an Android string resource templating library
+
+在项目中，我们经常会碰到一行文本，部分文字要显示不同的颜色和大小，或者其他样式。使用`StylePhrase`可以很方便的解决这个问题。只需要把需要显示其它样式的文本使用分割符包裹一下，就可以对文本进行各种操作。
+
+本库主要基于以下项目做了修改，更方便使用和扩展：
+
+https://github.com/square/phrase
+
+https://github.com/THEONE10211024/ColorPhrase
+
+# Preview
+
+![device-one-separator](./screenshot/device-one-separator.png)  ![device-one-separator](./screenshot/device-two-separator.png)
+
+![device-one-separator](./screenshot/device-multi-separator.png)  ![device-one-separator](./screenshot/device-custom-separator.png)
+
+# Usage
+
+**注意：对于比较特殊的字符，使用StylePhrase会导致一些问题，尽量使用{}、[]和()作为分割符**
+
+大部分时候，一行文本我们需要显示2种或3种样式，所以`StylePhrase`默认提供两种分割符`{}`和`[]`，当然你也可以修改默认的分割符。为了方便使用，`StylePhrase`默认提供了`setInnerFirstColor`、`setInnerFirstSize`、`setInnerSecondColor`、`setInnerSecondSize`
+
+一种操作符
+
+```kotlin
+// 永生不过是场{幻梦}，唯吾{所爱}不朽
+val oneSeparatorString = getString(R.string.text_phrase_one)
+val colorAndSize = StylePhrase(oneSeparatorString)
+                .setInnerFirstColor(Color.BLUE)
+                .setInnerFirstSize(20)
+tv_content.text = colorAndSize.format()
+```
+
+二种操作符
+
+```kotlin
+// 何以{缘起}?何以[缘灭]?当以{剑歌}问之
+val twoSeparatorString = getString(R.string.text_phrase_two)
+val colorAndSize = StylePhrase(twoSeparatorString)
+                .setInnerFirstColor(Color.BLUE)
+                .setInnerFirstSize(20)
+                .setInnerSecondColor(Color.RED)
+                .setInnerSecondSize(25)
+tv_content.text = colorAndSize.format()
+```
+
+多种操作符
+
+```kotlin
+// 凤兮{凤兮}归[故乡],遨(游)四海(求其凰)
+val colorAndSize = StylePhrase(multiSeparatorString)
+                .setInnerFirstColor(Color.BLUE)
+                .setInnerFirstSize(20)
+                .setInnerSecondColor(Color.RED)
+                .setInnerSecondSize(25)
+val builder = StylePhrase.Builder()
+builder.separator = "()"
+builder.setColor(Color.GREEN)
+builder.setSize(18)
+colorAndSize.addBuilder(builder)
+tv_content.text = colorAndSize.format()
+```
+
+更多操作可以参考DEMO
+
+# Download
+
+使用`Gradle`进行引用
+
+1.在你根目录的`build.gradle`文件添加代码: `maven { url 'https://www.jitpack.io' }`
+
+```groovy
+allprojects {
+    repositories {
+        ...
+        maven { url 'https://www.jitpack.io' }
+    }
+}
+```
+
+2.在你的工程目录添加
+
+`StylePhrase`依赖`appcompat-v7`，所以需要同时引用`appcompat-v7`包
+
+```groovy
+dependencies {
+	implementation 'com.github.yzbzz:StylePhrase:1.0.3'
+    implementation 'com.android.support:appcompat-v7:$version'
+}
+```
+
+你也可以直接下载`StylePhrase`类放到你的工程中
---
+++ app/src/main/java/com/shape/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.shape
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvOvalCode: TextView = findViewById(R.id.tv_oval_code)
+        val color = getColor("#ff4141")
+        ShapeInject.inject(tvOvalCode).setShapeType(ShapeInject.TYPE_OVAL).setStroke(2, color).background()
+
+        val btnRoundCode: Button = findViewById(R.id.tv_round_code)
+        val roundColor = getColor("#46b942")
+        val pressBgColor = getColor("#994897fa")
+        val disableBgColor = getColor("#cc999999")
+        val normalBgColor = getColor("#3b8fed")
+        ShapeInject.inject(btnRoundCode).setShapeType(ShapeInject.TYPE_ROUND).setStroke(2, roundColor)
+                .setBackgroundColor(pressBgColor, disableBgColor, normalBgColor).background()
+
+        val tvRoundRectCode: TextView = findViewById(R.id.tv_round_rect_code)
+        val roundRectColor = getColor("#4897fa")
+        ShapeInject.inject(tvRoundRectCode).setShapeType(ShapeInject.TYPE_ROUND_RECT).setStroke(2, roundRectColor).background()
+
+        val tvSegmentCode: TextView = findViewById(R.id.tv_segment_code)
+        val segmentColor = getColor("#ff4141")
+        ShapeInject.inject(tvSegmentCode).setShapeType(ShapeInject.TYPE_SEGMENT).setStroke(2, segmentColor).background()
+
+        val tvRadiusCode: TextView = findViewById(R.id.tv_radius_code)
+        val radiusColor = getColor("#ff00ff")
+        val radius = resources.getDimension(R.dimen.dp_10)
+        ShapeInject.inject(tvRadiusCode).setRadius(radius).setStroke(2, radiusColor).background()
+
+        val tvDashGapCode: TextView = findViewById(R.id.tv_dash_gap_code)
+        val dashGapPressedColor = getColor("#f7b218")
+        val dashGapNormalColor = getColor("#00ffff")
+        val dashGapSize = resources.getDimension(R.dimen.dp_5)
+        ShapeInject.inject(tvDashGapCode).setRadius(dashGapSize)
+                .setStroke(2, dashGapPressedColor, dashGapNormalColor, dashGapSize, dashGapSize)
+                .background()
+
+        val llAllCode: LinearLayout = findViewById(R.id.ll_all_code)
+        val tvAllCode: TextView = findViewById(R.id.tv_all_code)
+
+        val llNormalBgColor = getColor("#4897fa")
+        val llPressedBgColor = getColor("#ff4141")
+        val llNormalTextColor = getColor("#fdbc40")
+        val llPressedTextColor = getColor("#34c749")
+        val llStrokeNormalColor = getColor("#ff4141")
+        val llStrokePressColor = getColor("#4897fa")
+        val llSize = resources.getDimension(R.dimen.dp_10).toInt()
+        val llStrokeWidth = resources.getDimension(R.dimen.dp_2).toInt()
+        val radii = floatArrayOf(llSize.toFloat(), llSize.toFloat(), 0f, 0f, llSize.toFloat(), llSize.toFloat(), 0f, 0f)
+        ShapeInject.inject(llAllCode)
+                .setBackgroundColor(llPressedBgColor, llPressedBgColor, llNormalBgColor)
+                .setStroke(llStrokeWidth, llStrokePressColor, llStrokeNormalColor)
+                .setTextColor(llPressedTextColor, llPressedTextColor, llNormalTextColor, tvAllCode)
+                .setRadii(radii)
+                .background1()
+    }
+
+    private fun getColor(colorStr: String): Int {
+        return Color.parseColor(colorStr)
+    }
+}
